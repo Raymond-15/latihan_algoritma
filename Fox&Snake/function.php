b107f0bf343e54863b265ff914b50d05ce1153b6
Raymond-15 <raymondputra15@gmail.com>
2021-08-06 15:01:18 +0800

    update input method

--- Fox&Snake/function.php
+++ Fox&Snake/function.php
@@ -1,31 +1,33 @@
 <?php
 $hasil[] = "";
-$n = 5;
-$m = 3;
-$p = 1;
-$OK = false;
-for ($i = 1; $i <= $n; $i++) {
-    for ($j = 1; $j <= $m; $j++) {
-        if (($i % 2) != 0) {
-            $hasil[] =  "#";
-        } else {
-            if (($p % 2) != 0) {
-                if ($j != $m) {
-                    $hasil[] =  ".";
-                } else {
-                    $hasil[] = "#";
-                }
+if (isset($_POST['cek'])) {
+    $n = $_POST['n'];
+    $m = $_POST['m'];
+    $p = 1;
+    $OK = false;
+    for ($i = 1; $i <= $n; $i++) {
+        for ($j = 1; $j <= $m; $j++) {
+            if (($i % 2) != 0) {
+                $hasil[] =  "#";
             } else {
-                if ($j == 1) {
-                    $hasil[] = "#";
+                if (($p % 2) != 0) {
+                    if ($j != $m) {
+                        $hasil[] =  ".";
+                    } else {
+                        $hasil[] = "#";
+                    }
                 } else {
-                    $hasil[] = ".";
+                    if ($j == 1) {
+                        $hasil[] = "#";
+                    } else {
+                        $hasil[] = ".";
+                    }
                 }
             }
         }
+        if (($i % 2) == 0)
+            $p++;
+        $hasil[] = "<br>";
     }
-    if (($i % 2) == 0)
-        $p++;
-    $hasil[] = "<br>";
+    return 0;
 }
-return 0;

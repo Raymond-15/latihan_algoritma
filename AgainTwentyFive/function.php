441170ac55f6f7fad58ba7f72e9d1dbb991d4a92
Raymond-15 <raymondputra15@gmail.com>
2021-08-10 05:44:05 +0800

    coba metode lain

---
+++ AgainTwentyFive/function.php
@@ -0,0 +1,23 @@
+<?php
+$output = "";
+$hasil = "";
+if (isset($_POST['cek'])) {
+    $n = $_POST['input'];
+
+    // if ($n == 1) {
+    //     $output = floatval(5 % 100);
+    // }
+    // if ($n == 0) {
+    //     $output = floatval(1);
+    // }
+    // $x = floatval(($n / 2) % 100);
+    // if ($n % 2 == 0) {
+    //     $output = floatval(($x * $x) % 100);
+    // } else {
+    //     $output = floatval(($x * $x(5 % 100)) % 100);
+    // }
+
+    // $hasil = floatval($output % 100);
+
+    $hasil = 5;
+}
